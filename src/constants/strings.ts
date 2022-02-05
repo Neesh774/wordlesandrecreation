12e7173644ecacba7ab9095c4e8dff38ac482de6
Neesh774 <Kanishq0106@gmail.com>
2022-02-04 23:34:47 -0500

    initial commit

--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -1,12 +1,17 @@
-export const GAME_TITLE = 'GAME'
+export const GAME_TITLE = 'Wordles and Recreation'
 
-export const WIN_MESSAGES = ['Great Job!', 'Awesome', 'Well done!']
+export const WIN_MESSAGES = [
+  'You were born ready!',
+  'Awesome sauce!',
+  'Bababooey!',
+  "You might not know what you're doing, but you're doing it well!",
+]
 export const GAME_COPIED_MESSAGE = 'Game copied to clipboard'
 export const ABOUT_GAME_MESSAGE = 'About this game'
 export const NOT_ENOUGH_LETTERS_MESSAGE = 'Not enough letters'
 export const WORD_NOT_FOUND_MESSAGE = 'Word not found'
 export const CORRECT_WORD_MESSAGE = (solution: string) =>
-  `The word was ${solution}`
+  `The word was ${solution}. Sorry dude :(`
 export const ENTER_TEXT = 'Enter'
 export const DELETE_TEXT = 'Delete'
 export const STATISTICS_TITLE = 'Statistics'
